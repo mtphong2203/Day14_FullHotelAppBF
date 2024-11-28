d18146ac0c5fadab9759ca70f6e340b915bc523a
Tien Phong <tienphong2203@gmail.com>
2024-11-28 20:58:26 +0700

    FE: Create search hotel

--- hotel-app/src/client/src/modules/manager/hotel-service/hotel-service-list.component.ts
+++ hotel-app/src/client/src/modules/manager/hotel-service/hotel-service-list.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faSearch, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { HotelDetailsComponent } from './hotel-details/hotel-details.component';
 import { HttpClient } from '@angular/common/http';
 
@@ -16,18 +16,32 @@ import { HttpClient } from '@angular/common/http';
 export class HotelServiceListComponent implements OnInit {
 
   public isShowDetail: boolean = false;
+  public dataApi: any[] = [];
   public faPlus = faPlus;
   public faSearch = faSearch;
+  public faEdit = faEdit;
+  public faTrash = faTrash;
+  public keyword: string = '';
   public searchForm!: FormGroup;
 
   constructor(private http: HttpClient) {
 
   }
 
-  private apiURL: string = 'https://localhost:8080/api/manager/order'
+  private apiURL: string = 'http://localhost:8080/api/manager/order/search';
 
   ngOnInit(): void {
     this.createForm();
+    console.log(this.keyword);
+
+    this.search();
+  }
+
+  private search(): void {
+    this.http.get(this.apiURL).subscribe((data: any) => {
+      this.dataApi = data._embedded.orderDTOList;
+
+    })
   }
   private createForm(): void {
     this.searchForm = new FormGroup({
@@ -36,8 +50,9 @@ export class HotelServiceListComponent implements OnInit {
   }
 
   public onSubmit(): void {
-    console.log(this.searchForm.value);
-
+    this.apiURL = `http://localhost:8080/api/manager/order/search?keyword=${this.keyword}`;
+    console.log(this.keyword);
+    this.search();
   }
 
 }
