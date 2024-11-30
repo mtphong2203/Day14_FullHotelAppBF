6f029c084615eb964c1fee4b9ddf5aa039dda6b3
Tien Phong <tienphong2203@gmail.com>
2024-11-30 21:31:41 +0700

    FE: Create CRUD call APIs RoomService

--- hotel-app/src/client/src/modules/manager/room/room-details/room-details.component.ts
+++ hotel-app/src/client/src/modules/manager/room/room-details/room-details.component.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCancel, faRefresh, faSave, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-room-details',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, ReactiveFormsModule, CommonModule],
   templateUrl: './room-details.component.html',
   styleUrl: './room-details.component.css'
 })
-export class RoomDetailsComponent {
+export class RoomDetailsComponent implements OnChanges {
+
+  @Output() cancel: EventEmitter<void> = new EventEmitter<void>();
+  @Output() reSearch: EventEmitter<void> = new EventEmitter<void>();
+  @Input('editSelectItem') editSelectItem: any;
+  @Input('isEdit') isEdit: any;
+
+  // form control and api
+  public form!: FormGroup;
+  public apiURL = 'http://localhost:8080/api/v1/rooms';
+
+  //response
+  public message: string = '';
+
+  // boolean
+  public resetForm: boolean = true;
+
+  // icon
+  public faCancel: IconDefinition = faCancel;
+  public faRefresh: IconDefinition = faRefresh;
+  public faSave: IconDefinition = faSave;
+
+  constructor(private http: HttpClient) { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.createForm();
+    this.patchData();
+  }
+
+  private patchData(): void {
+    if (this.isEdit) {
+      this.form.patchValue(this.editSelectItem);
+    }
+  }
+
+  private createForm(): void {
+    this.form = new FormGroup({
+      number: new FormControl('', [Validators.required, Validators.maxLength(255)]),
+      type: new FormControl(null, Validators.required),
+      capacity: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      isActive: new FormControl(true),
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const data = this.form.value;
+    if (this.isEdit) {
+      if (this.form.value.number == this.editSelectItem.number) {
+        this.message = 'Number is can not unique';
+      } else {
+        this.http.put(`${this.apiURL}/${this.editSelectItem.id}`, data).subscribe((result: any) => {
+          if (result) {
+            this.message = 'Update success!';
+          } else {
+            this.message = 'Fail to update!';
+          }
+          this.onReset();
+        });
+      }
+    } else {
+      this.http.post(this.apiURL, data).subscribe((result: any) => {
+        if (result) {
+          this.message = 'Create success!';
+        } else {
+          this.message = 'Fail to create!';
+        }
+        this.onReset();
+      });
+    }
+  }
+
+  private onReset(): void {
+    this.reSearch.emit();
+    if (this.resetForm) {
+      this.form.reset();
+    }
+  }
+
+  public onCancel(): void {
+    this.cancel.emit();
+  }
+
 
 }
--- hotel-app/src/client/src/modules/manager/room/room-list.component.ts
+++ hotel-app/src/client/src/modules/manager/room/room-list.component.ts
@@ -1,12 +1,91 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch, IconDefinition, faPlus, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { RoomDetailsComponent } from "./room-details/room-details.component";
 @Component({
   selector: 'app-room-list',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, ReactiveFormsModule, CommonModule, RoomDetailsComponent],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.css'
 })
-export class RoomListComponent {
+export class RoomListComponent implements OnInit {
+
+  // form control and api
+  public searchForm!: FormGroup;
+  public apiURL = 'http://localhost:8080/api/v1/rooms';
+  public dataApi: any[] = [];
+
+  // boolean
+  public isShow: boolean = false;
+  public isEdit: boolean = false;
+
+  // edit object
+  public editSelect: any;
+
+  // icon
+  public faSearch: IconDefinition = faSearch;
+  public faPlus: IconDefinition = faPlus;
+  public faEdit: IconDefinition = faEdit;
+  public faTrash: IconDefinition = faTrash;
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.createForm();
+    this.search();
+  }
+
+  private createForm(): void {
+    this.searchForm = new FormGroup({
+      keyword: new FormControl('', Validators.required),
+    });
+  }
+
+  private search(): void {
+    this.http.get(this.apiURL).subscribe((data: any) => {
+      this.dataApi = data;
+    });
+  }
+
+  public onSubmit(): void {
+    if (this.searchForm.invalid) {
+      return;
+    }
+    this.apiURL = `http://localhost:8080/api/v1/rooms/searchByNumber?keyword=${this.searchForm.value.keyword}`;
+    this.search();
+  }
+
+  public onCreate(): void {
+    this.isShow = true;
+    this.isEdit = false;
+  }
+
+  public onCancel(): void {
+    this.isShow = false;
+  }
+
+  public reSearch(): void {
+    this.search();
+  }
+
+  public onEdit(id: string): void {
+    this.isShow = true;
+    this.editSelect = this.dataApi.find((item) => item.id === id);
+    this.isEdit = true;
+  }
+
+  public onDelete(id: string): void {
+    this.http.delete(`${this.apiURL}/${id}`).subscribe((result: any) => {
+      if (result) {
+        console.log('Deleted Success');
+      } else {
+        console.log('Fail to delete');
+      }
+      this.search();
+    });
+  }
 
 }
