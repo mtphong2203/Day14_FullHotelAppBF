39e1d55d368691b00ac2d3078ace2ef5167f3378
Tien Phong <tienphong2203@gmail.com>
2024-12-01 11:07:18 +0700

    FE: Config path for manager and fix URL

--- hotel-app/src/client/src/modules/manager/manager.module.ts
+++ hotel-app/src/client/src/modules/manager/manager.module.ts
@@ -3,19 +3,34 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RoomListComponent } from './room/room-list.component';
 import { HotelServiceListComponent } from './hotel-service/hotel-service-list.component';
+import { BookingListComponent } from './booking/booking-list.component';
+import { UserListComponent } from './user/user-list.component';
+import { RoleListComponent } from './role/role-list.component';
 
 const routes: Routes = [
+  {
+    path: 'users',
+    component: UserListComponent
+  },
+  {
+    path: 'roles',
+    component: RoleListComponent
+  },
+  {
+    path: 'bookings',
+    component: BookingListComponent
+  },
   {
     path: 'hotel-services',
     component: HotelServiceListComponent
   },
   {
-    path: 'room-list',
+    path: 'rooms',
     component: RoomListComponent,
   },
   {
     path: '**',
-    redirectTo: 'room-list',
+    redirectTo: 'rooms',
     pathMatch: 'full'
   }
 ]
