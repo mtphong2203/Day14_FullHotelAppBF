7ac5068c5424b5b4cf0917bc485b14771794b5e3
Tien Phong <tienphong2203@gmail.com>
2024-12-02 14:50:01 +0700

    FE: Update contact and layout customer

--- hotel-app/src/client/src/modules/customer/contact/contact.component.ts
+++ hotel-app/src/client/src/modules/customer/contact/contact.component.ts
@@ -2,17 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormatDatePipe } from '../../pipe/format-date.pipe';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPhoneAlt, faEnvelopeCircleCheck, faLocation, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, FormatDatePipe],
+  imports: [ReactiveFormsModule, CommonModule, FormatDatePipe, FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent implements OnInit {
   public contact!: FormGroup;
   public time: number = 90;
-  public isShow: Boolean = false;
+
+  // icon
+  public faPhone: IconDefinition = faPhoneAlt;
+  public faEmail: IconDefinition = faEnvelopeCircleCheck;
+  public faLocation: IconDefinition = faLocation;
+
+  // img
+  public image: string = './assets/images/contact.jpg';
 
   ngOnInit(): void {
     this.contact = new FormGroup({
@@ -25,12 +34,5 @@ export class ContactComponent implements OnInit {
   public onSubmit(): void {
     console.log(this.contact.value);
   }
-
-  public onCreate(): void {
-    this.isShow = !this.isShow;
-  }
-
-
-
 
 }
