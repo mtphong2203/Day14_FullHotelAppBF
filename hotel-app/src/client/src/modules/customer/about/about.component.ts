76f4ff5f26d18d2b8a1d7870b28054b7d428459f
Tien Phong <tienphong2203@gmail.com>
2024-12-02 15:26:08 +0700

    FE: Create view for about

--- hotel-app/src/client/src/modules/customer/about/about.component.ts
+++ hotel-app/src/client/src/modules/customer/about/about.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  public about01: string = './assets/images/about01.jpg';
+  public about02: string = './assets/images/room01.jpg';
+  public about03: string = './assets/images/room02.jpg';
 
 }
