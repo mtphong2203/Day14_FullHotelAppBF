df7bf43c640fb0750d33c5c096d4337065fbcac6
Tien Phong <tienphong2203@gmail.com>
2024-12-04 14:44:11 +0700

    FE: Add more navigate view

--- hotel-app/src/client/src/modules/common/header/header.component.ts
+++ hotel-app/src/client/src/modules/common/header/header.component.ts
@@ -1,15 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronCircleUp, faChevronCircleDown, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   public brandHome: string = './assets/images/brand-home.png';
+
+  public faDown: IconDefinition = faChevronCircleDown;
+  public faUp: IconDefinition = faChevronCircleUp;
+
+  public isHovered: boolean = false;
+  public onHover(status: boolean): void {
+    this.isHovered = status;
+  }
 
 }
